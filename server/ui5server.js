64b61e45f2afdf31c2addb9393adefa820f98d19
PeterOrgon <peter.orgon@outlook.com>
2017-11-24 23:07:29 +0100

    Config processing change

--- server/ui5server.js
+++ server/ui5server.js
@@ -8,6 +8,13 @@ const routes = require('express').Router();
 const serverConfig = require("./config.json");
 
 var config = serverConfig;
+var cnsColors = {
+  reset: '\x1b[0m',
+  success: '\x1b[32m',
+  error: '\x1b[31m',
+  warning: '\x1b[33m',
+  emphasize: '\x1b[1m'
+}
 
 try {
     var port = config.port;
@@ -22,8 +29,8 @@ try {
     app.use(express.static(path.join(__dirname, '..', appsDir)));
 
     var proxy = httpProxy.createProxyServer({});
-
     var target = '';
+    
     proxy.on('proxyReq', function(proxyReq, req, res, options) {
         proxyReq.path = target;
 
@@ -41,7 +48,8 @@ try {
 
 
     proxy.on('error', function(err, preq, pres) {
-        console.log('Proxy error: \n', err);
+        console.log(cnsColors.error, 'Proxy error occured.');
+        console.log(cnsColors.emphasize, err, cnsColors.reset);
     });
 
     var proxyHost = '';
@@ -49,14 +57,37 @@ try {
     routes.route(/^\/(.+)/).all(function(req, res, next) {
 
         proxyTarget = config.services.find(function(service) {
-            return req.url.includes(service.url);
+            return req.url.includes(service.path);
         });
 
         if (proxyTarget) {
-            if (proxyTarget.url) {
-                var urlPath = req.url.replace(proxyTarget.originProxyPartPath, proxyTarget.targetProxyPartPath);
-                proxyHost = proxyTarget.targetHost;
-                target = proxyHost + urlPath;
+            if (proxyTarget.path) {
+
+                var targetPath = proxyTarget.path;
+                if(proxyTarget.pathRewrite) {
+                  for (var rewriteKey in proxyTarget.pathRewrite){
+                      if (proxyTarget.pathRewrite.hasOwnProperty(rewriteKey)) {
+                          try {
+                              new RegExp(rewriteKey);
+                          }
+                          catch(e) {
+                              console.log(cnsColors.error, 'Configuration error occured.');
+                              console.log(cnsColors.emphasize, 'Invalid regular expression in "' + rewriteKey + '". Please review your configuration.', cnsColors.reset);
+                              return res.status(500).json('Invalid regular expression in ' + rewriteKey + '. Please review your configuration.');  
+                          }
+                          targetPath = targetPath.replace(new RegExp( rewriteKey ), proxyTarget.pathRewrite[rewriteKey]);
+                      }
+                  } 
+                }
+
+                var proxyHost = proxyTarget.targetHost;
+                target = proxyHost + targetPath;
+
+                console.log(cnsColors.emphasize,'Requesting: ', target, cnsColors.reset);
+
+                if (!proxyTarget.targetHeaders) {
+                  proxyTarget.targetHeaders = {};
+                }
 
                 proxy.web(req, res, {
                     target: target,
@@ -72,15 +103,14 @@ try {
         } else {
             return next();
         }
-
-
     });
 
     app.use('/', routes);
     app.listen(port);
 
-    console.log('UI5 Server listening on port %s ...', port);
+    console.log(cnsColors.success, 'UI5 Server listening on port ' + port + ' ...', cnsColors.reset);
 } catch (err) {
-    console.log('Somehing bad happened ... \n', err);
+    console.log(cnsColors.error, 'Somehing bad happened ... ');
+    console.log(cnsColors.emphasize, err, cnsColors.reset);
 }
 
